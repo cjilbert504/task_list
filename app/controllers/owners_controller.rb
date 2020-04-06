3fc375a302c81a1beb98b7fd73a9a0a5db21093d
Collin Jilbert <cjilbert504@gmail.com>
2020-04-05 23:01:14 -0500

    ensuring object associations work properly

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,8 @@
 class OwnersController < ApplicationController
+
+    def show 
+        @owner = Owner.first
+        @list = @owner.lists.first
+        @tasks = @list.tasks
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+o = Owner.create(name: "Todd")
+o.lists << l = List.create(name: "Monday")
+e = Employee.create(name: "Collin")
+t = Task.new(content: "interior cleaning")
+o.tasks << t 
+l.tasks << t 
+e.tasks << t 
